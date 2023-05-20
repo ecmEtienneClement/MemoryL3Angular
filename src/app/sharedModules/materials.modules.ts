e6877e3c81447fb9d996d0bee916f6a5e13a01cd
moussadiagne036 <moussadiagne036@gmail.com>
2023-05-20 21:32:54 +0000

    mise a jour des redirections

--- src/app/sharedModules/materials.modules.ts
+++ src/app/sharedModules/materials.modules.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import {MatTableModule} from '@angular/material/table';
+
 @NgModule({
   exports: [
     MatFormFieldModule,
