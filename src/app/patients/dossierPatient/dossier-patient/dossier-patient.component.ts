4e5500fa8050b750b9a22d3f798adc593dbc564e
moussadiagne036 <moussadiagne036@gmail.com>
2023-05-30 20:36:21 +0000

    05 : modification dossier patient : en cour

--- src/app/patients/dossierPatient/dossier-patient/dossier-patient.component.ts
+++ src/app/patients/dossierPatient/dossier-patient/dossier-patient.component.ts
@@ -14,6 +14,41 @@ import {
   templateUrl: './dossier-patient.component.html',
   styleUrls: ['./dossier-patient.component.scss'],
 })
-export class DossierPatientComponent {
-  panelOpenState = false ;
+export class DossierPatientComponent implements OnInit {
+  stateApp$: Observable<StateApp> = new Observable<StateApp>();
+  dossierPatient: DossierPatient[] = [];
+  notification: string[] = [];
+  errorMessage: string[] = [];
+  id:any=''
+  //
+  readonly routesName = RoutesNames;
+  panelOpenState = false;
+
+  constructor(
+    private store: Store<AppState>,
+    private dossierPatientsActions: DossierPatientsActions,
+    private dossierPatientsSelectors: DossierPatientsSelectors
+  ) {}
+  //
+  ngOnInit() {
+    this.id = localStorage.getItem('idRoute');
+    this.store.dispatch(this.dossierPatientsActions.getAllEntities()());
+    //
+    this.store.select(this.dossierPatientsSelectors.getEntities()).subscribe({
+      next: (data) => {
+        this.dossierPatient = data.filter((data) => data.patient == this.id);
+      },
+    });
+    this.stateApp$ = this.store.select(
+      this.dossierPatientsSelectors.getStateApp()
+    );
+    this.store
+      .select(this.dossierPatientsSelectors.getNotification())
+      .subscribe({
+        next: (data) => (this.notification = data),
+      });
+    this.store.select(this.dossierPatientsSelectors.getError()).subscribe({
+      next: (data) => (this.errorMessage = data),
+    });
+  }
 }
